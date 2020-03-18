b5e327ec3a31faf04a20d56b40176384a3fd19dc
Gabriel Pérez <gabriel@garox.org>
2020-03-18 19:46:06 -0300

    fix range key

--- src/util/getModelForClass.ts
+++ src/util/getModelForClass.ts
@@ -17,6 +17,20 @@ export const getModelForClass = <T extends instanceOfDynamoDBClass>({
 }: DynamoDBClassWithOptions) => {
   const table = getTable(dynamoDBClass)
   const awsSchema = getSchema(new dynamoDBClass())
-  const schema = new Schema(awsSchema, schemaOptions)
+
+  const { hash, range } = getKeys(awsSchema)
+  let hashObject = awsSchema[hash]
+  let rangeObject = awsSchema[range]
+  delete (hashObject as any).keyType
+  delete (rangeObject as any).keyType
+
+  const schema = new Schema(
+    {
+      ...awsSchema,
+      [hash]: { ...hashObject, hashKey: true },
+      [range]: { ...rangeObject, rangeKey: true },
+    },
+    schemaOptions,
+  )
   return model<T, {}>(table, schema, modelOptions)
 }
